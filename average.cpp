b2f0afff75b69357185c7cfddcf4d5378672300a
Teetad <teetadraseesit@gmail.com>
2025-02-21 14:59:08 +0700

    complete

--- average.cpp
+++ average.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+#include <cstdlib>
 
+int main(int num,char* input[]){
+    if (num < 2)
+    {
+        std::cout << "Please input numbers to find average.";
+        return 0;
+    }else{
+        double a;
+        double b; 
+        for (int i = 1; i < num; i++){
+            a = atof(input[i]);
+            b += a;
+        }
+        b = b/(num-1);
+        std::cout << "---------------------------------" << std::endl;
+        std::cout << "Average of "<< (num-1)<< " numbers = " << b << std::endl;
+        std::cout << "---------------------------------" << std::endl;    
+    }
+
+}
--- calculator.cpp
+++ calculator.cpp
@@ -1,8 +1,67 @@
 #include <windows.h>
+#include <iostream>
+#include <cstdlib>
+#include <stdio.h>
 
+char input1[100],input2[100],ans[100];
+HWND text,fill1,fill2,button_plus,button_minus,button_multi,button_div;
+double num1,num2,sum;
 /* This is where all the input to the window goes to */
 LRESULT CALLBACK WndProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam) {
 	switch(Message) {
+		
+		case WM_CREATE: 
+			text = CreateWindow("STATIC", "please input two nubers",WS_VISIBLE | WS_CHILD | WS_BORDER,40,10,175,20,hwnd, NULL,NULL,NULL);
+			fill1 = CreateWindow("EDIT", "",WS_VISIBLE | WS_CHILD | WS_BORDER,50,50,140,20,hwnd, (HMENU) 5,NULL,NULL);
+			fill2 = CreateWindow("EDIT", "",WS_VISIBLE | WS_CHILD | WS_BORDER,50,90,140,20,hwnd, (HMENU) 6,NULL,NULL);
+			button_plus = CreateWindow("BUTTON", "+",WS_VISIBLE | WS_CHILD | WS_BORDER,30,130,30,20,hwnd, (HMENU) 1,NULL,NULL);
+			button_minus = CreateWindow("BUTTON", "-",WS_VISIBLE | WS_CHILD | WS_BORDER,80,130,30,20,hwnd, (HMENU) 2,NULL,NULL);
+			button_multi = CreateWindow("BUTTON", "*",WS_VISIBLE | WS_CHILD | WS_BORDER,130,130,30,20,hwnd, (HMENU) 3,NULL,NULL);
+			button_div = CreateWindow("BUTTON", "/",WS_VISIBLE | WS_CHILD | WS_BORDER,180,130,30,20,hwnd, (HMENU) 4,NULL,NULL);
+			break;
+
+		case WM_COMMAND: 
+			GetWindowText(GetDlgItem(hwnd, 5), input1, sizeof(input1));
+			GetWindowText(GetDlgItem(hwnd, 6), input2, sizeof(input2));
+			num1 = atof(input1);
+			num2 = atof(input2);
+			switch (LOWORD(wParam))
+			{
+			case 1:
+				sum = num1 + num2;
+				sprintf(ans, "%.2f",sum);
+				MessageBox(hwnd, ans, "Result", MB_OK);
+				break;
+			case 2:
+				sum = num1 - num2;
+				sprintf(ans, "%.2f",sum);
+				MessageBox(hwnd, ans, "Result", MB_OK);
+				break;
+			case 3:
+				sum = num1 * num2;
+				sprintf(ans, "%.2f",sum);
+				MessageBox(hwnd, ans, "Result", MB_OK);
+			case 4:
+				if (num2 == 0)
+				{	
+					if (num1 != 0)
+					{
+					MessageBox(hwnd, "Cannot divide by zero", "Result", MB_OK | MB_OK);
+					return 0;
+					}else{
+		
+					MessageBox(hwnd, ans, "Result", MB_OK);
+					return 0;
+					}
+				}
+				sum = num1 / num2;
+					sprintf(ans, "%.2f",sum);
+					MessageBox(hwnd, ans, "Result", MB_OK);
+					break;
+				
+			
+			}
+			break;
 		
 		/* Upon destruction, tell the main thread to stop */
 		case WM_DESTROY: {
@@ -31,7 +90,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	wc.hCursor	 = LoadCursor(NULL, IDC_ARROW);
 	
 	/* White, COLOR_WINDOW is just a #define for a system color, try Ctrl+Clicking it */
-	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);
+	wc.hbrBackground = (HBRUSH)CreateSolidBrush(RGB(255,0,0));
 	wc.lpszClassName = "WindowClass";
 	wc.hIcon	 = LoadIcon(NULL, IDI_APPLICATION); /* Load a standard icon */
 	wc.hIconSm	 = LoadIcon(NULL, IDI_APPLICATION); /* use the name "A" to use the project icon */
@@ -41,11 +100,11 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		return 0;
 	}
 
-	hwnd = CreateWindowEx(WS_EX_CLIENTEDGE,"WindowClass","Caption",WS_VISIBLE|WS_OVERLAPPEDWINDOW,
+	hwnd = CreateWindowEx(WS_EX_CLIENTEDGE,"WindowClass","My Calculator",WS_VISIBLE|WS_SYSMENU,
 		CW_USEDEFAULT, /* x */
 		CW_USEDEFAULT, /* y */
-		640, /* width */
-		480, /* height */
+		250, /* width */
+		200, /* height */
 		NULL,NULL,hInstance,NULL);
 
 	if(hwnd == NULL) {
